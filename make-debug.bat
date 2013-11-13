d6970a6e77bac84d4560ca426dbd0394ab5d30f9
Henry Slawniak <meggawatts@gmail.com>
2013-11-13 00:48:17 -0600

    Sound now provided by another submodule

--- make-debug.bat
+++ make-debug.bat
@@ -3,7 +3,9 @@ cd %appdata%\.minecraft\resourcepacks
 SET newDir=%CD%
 cd %oldDir%
 if not exist %newDir%\Gondor-DEBUG mkdir %newDir%\Gondor-DEBUG
+del %newdir%\Gondor-DEBUG\*.* /s /q
 xcopy /y/s %oldDir%\src\main\meta\* %newDir%\Gondor-DEBUG
 xcopy /y/s %oldDir%\src\main\pack\* %newDir%\Gondor-DEBUG\assets\minecraft
 xcopy /y/s %oldDir%\MCME-UI\src\main\pack\*.* %newDir%\Gondor-DEBUG\assets\minecraft
+xcopy /y/s %oldDir%\MCME-Aural-Experience\src\main\pack\*.* %newDir%\Gondor-DEBUG\assets\minecraft
 pause
