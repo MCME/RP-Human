a18a5a71c1b2a0ae5d25b2ebcf56eb5aabca288a
Henry Slawniak <meggawatts@gmail.com>
2014-04-19 13:33:57 -0500

    Update debug script

--- make-debug.bat
+++ make-debug.bat
@@ -2,10 +2,9 @@ SET oldDir=%CD%
 cd %appdata%\.minecraft\resourcepacks
 SET newDir=%CD%
 cd %oldDir%
-if not exist %newDir%\Gondor-DEBUG mkdir %newDir%\Gondor-DEBUG
-del %newdir%\Gondor-DEBUG\*.* /s /q
-xcopy /y/s %oldDir%\src\main\meta\* %newDir%\Gondor-DEBUG
-xcopy /y/s %oldDir%\src\main\pack\* %newDir%\Gondor-DEBUG\assets\minecraft
-xcopy /y/s %oldDir%\MCME-UI\src\main\pack\*.* %newDir%\Gondor-DEBUG\assets\minecraft
-xcopy /y/s %oldDir%\MCME-Aural-Experience\src\main\pack\*.* %newDir%\Gondor-DEBUG\assets\minecraft
-pause
+if not exist %newDir%\MCME-Gondor-DEBUG mkdir %newDir%\MCME-Gondor-DEBUG
+del %newdir%\MCME-Gondor-DEBUG\*.* /s /q
+xcopy /y/s %oldDir%\src\main\meta\* %newDir%\MCME-Gondor-DEBUG
+xcopy /y/s %oldDir%\src\main\pack\* %newDir%\MCME-Gondor-DEBUG\assets\minecraft
+xcopy /y/s %oldDir%\MCME-UI\src\main\pack\*.* %newDir%\MCME-Gondor-DEBUG\assets\minecraft
+xcopy /y/s %oldDir%\MCME-Aural-Experience\src\main\pack\*.* %newDir%\MCME-Gondor-DEBUG\assets\minecraft
