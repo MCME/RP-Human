333f51c11841e21b8fedd84016c61132aacbb06e
TotiGonzales <dntstudios101@gmail.com>
2023-08-02 21:46:26 +0200

    Branch start.
    Signed-off-by: TotiGonzales <dntstudios101@gmail.com>

---
+++ assets/mcme/models/renamer.py
@@ -0,0 +1,21 @@
+import os
+
+def duplicate_and_replace(folder_path):
+    for filename in os.listdir(folder_path):
+        if filename.startswith('oak'):
+            oak_file_path = os.path.join(folder_path, filename)
+            maple_file_path = os.path.join(folder_path, filename.replace('oak', 'beech'))
+
+            # Duplicate the file
+            with open(oak_file_path, 'r') as oak_file:
+                oak_content = oak_file.read()
+            with open(maple_file_path, 'w') as maple_file:
+                maple_file.write(oak_content.replace('oak', 'beech'))
+
+            print(f'Duplicated and replaced: {filename} -> {filename.replace("oak", "beech")}')
+
+if __name__ == "__main__":
+    script_directory = os.path.dirname(__file__)
+    folder_name = "block"  # Replace with the name of your folder
+    folder_path = os.path.join(script_directory, folder_name)
+    duplicate_and_replace(folder_path)
