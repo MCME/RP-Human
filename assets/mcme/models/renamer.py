f68c997f92f2b4259c03ea7a5044d13e3a85ec49
TotiGonzales <dntstudios101@gmail.com>
2023-08-10 22:12:47 +0200

    Birch stuffs.
    Signed-off-by: TotiGonzales <dntstudios101@gmail.com>

--- assets/mcme/models/renamer.py
+++ assets/mcme/models/renamer.py
@@ -2,17 +2,17 @@ import os
 
 def duplicate_and_replace(folder_path):
     for filename in os.listdir(folder_path):
-        if filename.startswith('oak'):
+        if filename.startswith('beech'):
             spruce_file_path = os.path.join(folder_path, filename)
-            maple_file_path = os.path.join(folder_path, filename.replace('oak', 'chestnut'))
+            maple_file_path = os.path.join(folder_path, filename.replace('beech', 'birch'))
 
             # Duplicate the file
             with open(spruce_file_path, 'r') as spruce_file:
                 spruce_content = spruce_file.read()
             with open(maple_file_path, 'w') as maple_file:
-                maple_file.write(spruce_content.replace('oak', 'chestnut'))
+                maple_file.write(spruce_content.replace('beech', 'birch'))
 
-            print(f'Duplicated and replaced: {filename} -> {filename.replace("oak", "chestnut")}')
+            print(f'Duplicated and replaced: {filename} -> {filename.replace("beech", "birch")}')
 
 if __name__ == "__main__":
     script_directory = os.path.dirname(__file__)
