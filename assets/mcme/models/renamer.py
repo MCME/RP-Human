aad1cd168fb9741ad733cd33bd74402ec89906c9
TotiGonzales <dntstudios101@gmail.com>
2023-08-15 01:45:34 +0200

    Poplar finalization, fixes and adjustments, added ash.
    Signed-off-by: TotiGonzales <dntstudios101@gmail.com>

--- assets/mcme/models/renamer.py
+++ assets/mcme/models/renamer.py
@@ -4,15 +4,15 @@ def duplicate_and_replace(folder_path):
     for filename in os.listdir(folder_path):
         if filename.startswith('pine'):
             spruce_file_path = os.path.join(folder_path, filename)
-            maple_file_path = os.path.join(folder_path, filename.replace('pine', 'maple'))
+            maple_file_path = os.path.join(folder_path, filename.replace('pine', 'ash'))
 
             # Duplicate the file
             with open(spruce_file_path, 'r') as spruce_file:
                 spruce_content = spruce_file.read()
             with open(maple_file_path, 'w') as maple_file:
-                maple_file.write(spruce_content.replace('pine', 'maple'))
+                maple_file.write(spruce_content.replace('pine', 'ash'))
 
-            print(f'Duplicated and replaced: {filename} -> {filename.replace("pine", "maple")}')
+            print(f'Duplicated and replaced: {filename} -> {filename.replace("pine", "ash")}')
 
 if __name__ == "__main__":
     script_directory = os.path.dirname(__file__)
