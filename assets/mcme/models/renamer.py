26efdcff7f0bf3f74fccc4ba54d83714fc7c2d58
TotiGonzales <dntstudios101@gmail.com>
2023-08-17 01:18:30 +0200

    Fixes
    Signed-off-by: TotiGonzales <dntstudios101@gmail.com>

--- assets/mcme/models/renamer.py
+++ assets/mcme/models/renamer.py
@@ -2,17 +2,17 @@ import os
 
 def duplicate_and_replace(folder_path):
     for filename in os.listdir(folder_path):
-        if filename.startswith('spruce_leaves'):
+        if filename.startswith('mossy_larch'):
             pine_file_path = os.path.join(folder_path, filename)
-            maple_file_path = os.path.join(folder_path, filename.replace('spruce_leaves', 'spruce_leaves_alt'))
+            maple_file_path = os.path.join(folder_path, filename.replace('mossy_larch', 'mossy_oak'))
 
             # Duplicate the file
             with open(pine_file_path, 'r') as pine_file:
                 pine_content = pine_file.read()
             with open(maple_file_path, 'w') as maple_file:
-                maple_file.write(pine_content.replace('spruce_leaves', 'spruce_leaves_alt'))
+                maple_file.write(pine_content.replace('mossy_larch', 'mossy_oak'))
 
-            print(f'Duplicated and replaced: {filename} -> {filename.replace("spruce_leaves", "spruce_leaves_alt")}')
+            print(f'Duplicated and replaced: {filename} -> {filename.replace("mossy_larch", "mossy_oak")}')
 
 if __name__ == "__main__":
     script_directory = os.path.dirname(__file__)
