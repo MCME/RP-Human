f670a8a3b4f626c2c784e156f12feb356f61ee66
TotiGonzales <dntstudios101@gmail.com>
2023-08-17 01:54:30 +0200

    Mossy maple.
    Signed-off-by: TotiGonzales <dntstudios101@gmail.com>

--- assets/mcme/models/renamer.py
+++ assets/mcme/models/renamer.py
@@ -4,15 +4,15 @@ def duplicate_and_replace(folder_path):
     for filename in os.listdir(folder_path):
         if filename.startswith('mossy_larch'):
             pine_file_path = os.path.join(folder_path, filename)
-            maple_file_path = os.path.join(folder_path, filename.replace('mossy_larch', 'mossy_oak'))
+            maple_file_path = os.path.join(folder_path, filename.replace('mossy_larch', 'mossy_maple'))
 
             # Duplicate the file
             with open(pine_file_path, 'r') as pine_file:
                 pine_content = pine_file.read()
             with open(maple_file_path, 'w') as maple_file:
-                maple_file.write(pine_content.replace('mossy_larch', 'mossy_oak'))
+                maple_file.write(pine_content.replace('mossy_larch', 'mossy_maple'))
 
-            print(f'Duplicated and replaced: {filename} -> {filename.replace("mossy_larch", "mossy_oak")}')
+            print(f'Duplicated and replaced: {filename} -> {filename.replace("mossy_larch", "mossy_maple")}')
 
 if __name__ == "__main__":
     script_directory = os.path.dirname(__file__)
