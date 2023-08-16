3e60c75486e64f9b21c8b7486a5a3be39749b291
TotiGonzales <dntstudios101@gmail.com>
2023-08-16 02:54:42 +0200

    Birch tweaks. Spruce full block.
    Signed-off-by: TotiGonzales <dntstudios101@gmail.com>

--- assets/mcme/models/renamer.py
+++ assets/mcme/models/renamer.py
@@ -2,17 +2,17 @@ import os
 
 def duplicate_and_replace(folder_path):
     for filename in os.listdir(folder_path):
-        if filename.startswith('elm'):
+        if filename.startswith('spruce_leaves'):
             pine_file_path = os.path.join(folder_path, filename)
-            maple_file_path = os.path.join(folder_path, filename.replace('elm', 'hornbeam'))
+            maple_file_path = os.path.join(folder_path, filename.replace('spruce_leaves', 'spruce_leaves_alt'))
 
             # Duplicate the file
             with open(pine_file_path, 'r') as pine_file:
                 pine_content = pine_file.read()
             with open(maple_file_path, 'w') as maple_file:
-                maple_file.write(pine_content.replace('elm', 'hornbeam'))
+                maple_file.write(pine_content.replace('spruce_leaves', 'spruce_leaves_alt'))
 
-            print(f'Duplicated and replaced: {filename} -> {filename.replace("elm", "hornbeam")}')
+            print(f'Duplicated and replaced: {filename} -> {filename.replace("spruce_leaves", "spruce_leaves_alt")}')
 
 if __name__ == "__main__":
     script_directory = os.path.dirname(__file__)
