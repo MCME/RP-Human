774be4f1ae194d01615d6b0d003838770c5a7b23
TotiGonzales <dntstudios101@gmail.com>
2023-08-18 03:53:44 +0200

    Linden leaves. Larch adjustments.
    Signed-off-by: TotiGonzales <dntstudios101@gmail.com>

--- assets/mcme/models/renamer.py
+++ assets/mcme/models/renamer.py
@@ -2,17 +2,17 @@ import os
 
 def duplicate_and_replace(folder_path):
     for filename in os.listdir(folder_path):
-        if filename.startswith('beech_leaves'):
+        if filename.startswith('linden_leaves'):
             pine_file_path = os.path.join(folder_path, filename)
-            maple_file_path = os.path.join(folder_path, filename.replace('beech_leaves', 'ash_leaves_yellowish'))
+            maple_file_path = os.path.join(folder_path, filename.replace('linden_leaves', 'linden_leaves_yellowish'))
 
             # Duplicate the file
             with open(pine_file_path, 'r') as pine_file:
                 pine_content = pine_file.read()
             with open(maple_file_path, 'w') as maple_file:
-                maple_file.write(pine_content.replace('beech_leaves', 'ash_leaves_yellowish'))
+                maple_file.write(pine_content.replace('linden_leaves', 'linden_leaves_yellowish'))
 
-            print(f'Duplicated and replaced: {filename} -> {filename.replace("beech_leaves", "ash_leaves_yellowish")}')
+            print(f'Duplicated and replaced: {filename} -> {filename.replace("linden_leaves", "linden_leaves_yellowish")}')
 
 if __name__ == "__main__":
     script_directory = os.path.dirname(__file__)
