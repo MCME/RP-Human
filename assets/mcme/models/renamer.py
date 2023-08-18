71309c808671a5305ea25242b5985b8a12f27c51
TotiGonzales <dntstudios101@gmail.com>
2023-08-18 02:46:57 +0200

    Leaf colors.
    Signed-off-by: TotiGonzales <dntstudios101@gmail.com>

--- assets/mcme/models/renamer.py
+++ assets/mcme/models/renamer.py
@@ -2,17 +2,17 @@ import os
 
 def duplicate_and_replace(folder_path):
     for filename in os.listdir(folder_path):
-        if filename.startswith('spruce'):
+        if filename.startswith('beech_leaves'):
             pine_file_path = os.path.join(folder_path, filename)
-            maple_file_path = os.path.join(folder_path, filename.replace('spruce', 'larch'))
+            maple_file_path = os.path.join(folder_path, filename.replace('beech_leaves', 'ash_leaves_yellowish'))
 
             # Duplicate the file
             with open(pine_file_path, 'r') as pine_file:
                 pine_content = pine_file.read()
             with open(maple_file_path, 'w') as maple_file:
-                maple_file.write(pine_content.replace('spruce', 'larch'))
+                maple_file.write(pine_content.replace('beech_leaves', 'ash_leaves_yellowish'))
 
-            print(f'Duplicated and replaced: {filename} -> {filename.replace("spruce", "larch")}')
+            print(f'Duplicated and replaced: {filename} -> {filename.replace("beech_leaves", "ash_leaves_yellowish")}')
 
 if __name__ == "__main__":
     script_directory = os.path.dirname(__file__)
