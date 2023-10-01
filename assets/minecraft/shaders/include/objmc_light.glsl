52b67c6e3f9bba0947d187f178667e812f7510e6
barteldvn <barteldvn@gmail.com>
2023-10-01 21:38:25 +0200

    scuffed ferns, big ferns look good, lighting tests.

--- assets/minecraft/shaders/include/objmc_light.glsl
+++ assets/minecraft/shaders/include/objmc_light.glsl
@@ -10,9 +10,13 @@ else if (noshadow == 0) {
 
     //block lighting
     #ifdef BLOCK
-    float vertical = sign(normal.y) * 0.3 + 0.7;
-    float horizontal = abs(normal.z) * 0.25 + 0.5;
-    float brightness =  0.06 + mix(horizontal, vertical, abs((normal.y) * 0.57));
+    float vertical = sign(normal.y) * 0.22 + 0.5;
+    float horizontal = abs(normal.z) * 0.15 + 0.7;
+	float undershadow =  abs(normal.y);
+	if ((normal.y) < 0.7) {
+	    undershadow = undershadow * 0.69;
+	}
+    float brightness = 0.03 + mix(horizontal, vertical, undershadow);
     color *= vec4(vec3(brightness), 1.0);
     #endif
 	
@@ -23,8 +27,23 @@ else if (noshadow == 0) {
     if (isGUI == 1) normal.y *= -1;
     color *= minecraft_mix_light(Light0_Direction, Light1_Direction, normal, overlayColor);
     #endif
-    vec4 minLightColor = vec4(0.7, 0.7, 0.7, 0.7); // Adjust these values
-    vec4 maxLightColor = vec4(1.0, 1.0, 1.0, 1.0); // Adjust these values
-    vec4 lightColor2 = clamp(lightColor, minLightColor, maxLightColor);
-    color *= lightColor * ColorModulator;
+    vec4 lightColor2 = lightColor;
+	// Assuming lightColor2 is an RGB vec4 representing the light color
+	vec3 lightColorRGB = lightColor2.rgb;
+
+	// Calculate the luminance of the fragment color
+	float luminance = dot(lightColor.rgb, vec3(0.199, 0.587, 0.414));
+
+	// Define a factor to control the amount of darkening in darker areas
+	float darkeningFactor = 0.4; // Adjust this factor as needed
+
+	// Apply darkening only in darker areas (luminance < threshold)
+	if (luminance < 20.0) {
+		lightColorRGB *= darkeningFactor;
+	}
+
+	// Reconstruct the final color with the modified lightColor2
+	//lightColor2.rgb = lightColorRGB;
+	//lightColor2.w *= 1.8;
+    color *= lightColor2 * ColorModulator;
 }
